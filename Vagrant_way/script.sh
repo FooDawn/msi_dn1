894d10b0398f17039f82c3162cff76f29f73cf88
FooDawn <mal.medo987@gmail.com>
2021-12-13 17:47:34 +0100

    začetek dela z colud init

--- Vagrant_way/script.sh
+++ Vagrant_way/script.sh
@@ -1,9 +1,9 @@
 echo "Updating and upgrading"
 apt-get update > /dev/null
 apt-get upgrade -y > /dev/null
-export DEBIAN_FRONTEND=noninteractive
 echo "Installing debconf-utils"
 apt-get install debconf-utils -y > /dev/nul
+export DEBIAN_FRONTEND=noninteractive
 
 echo "Installing i3 Window Manager"
 apt-get install xorg -y > /dev/nul
@@ -12,11 +12,13 @@ apt-get install i3 -y > /dev/nul
 echo "exec i3" >> ~/.xinitrc
 
 echo "Installing docker so that gns3 will be able to run currectly"
+apt-get remove -y docker docker-engine docker.io containerd runc
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common > /dev/null
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg > sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" > /dev/nul
 apt-cache policy docker-ce > /dev/nul
 apt-get install docker-ce -y > /dev/nul
+
 
 # to check if it's running type: sudo systemctl status docker 
 
@@ -26,7 +28,7 @@ echo "wireshark-common wireshark-common/install-setuid boolean true" | debconf-s
 apt-get install gns3-gui gns3-server -y > /dev/nul
 
 # ADD NON-SUDO USER TO GNS3, UBRINGE
-usermod -aG ubridge,libvirt,kvm vagrant
+usermod -aG ubridge,libvirt,kvm,docker vagrant
 
 echo "Installing midori browser"
 apt-get install midori -y > /dev/nul
